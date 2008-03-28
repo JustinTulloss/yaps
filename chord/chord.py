b9145cad904c1b5c0c4059ae930cefce759f4097
Justin Tulloss <jmtulloss@gmail.com>
2008-03-27 22:12:45 -0500

    Can pickle an object and send it to a server (right now statically sends to localhost
    --HG--
    branch : trunk

--- chord/chord.py
+++ chord/chord.py
@@ -1,15 +1,21 @@
 """
-Implements a chord-like distributed hash table in python
+Implements a chord-like distributed hash table in python. This is purely
+for storing python objects.
 
 Justin Tulloss
 """
 import logging
 import socket
+import struct
+from struct import Struct
+import pickle
 import threading
 import hashlib
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
+
+packet = Struct('!40sh')
 
 class Chord(object):
     def __init__(self, localstore = True, **kwargs):
@@ -21,15 +27,19 @@ class Chord(object):
             self._ls = ChordServer()
 
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # key - size - value (network endianness)
 
     def __getitem__(self, key):
-        log.debug(key)
+        pass
 
     def __setitem__(self, key, value):
         sha = hashlib.sha1(key)
 
+        pvalue = pickle.dumps(value)
+        msg = packet.pack(sha.hexdigest(), len(pvalue))
+        msg = "%s%s" % (msg, pvalue)
         self._sock.connect(('localhost', 4090))
-        self._sock.send(sha.digest())
+        self._sock.send(msg)
 
     def __delitem__(self, key):
         pass
@@ -56,8 +66,23 @@ class ChordServer(object):
         self._accept_thread.setDaemon(True)
         self._accept_thread.start()
 
-    def accept_loop(self):
-        while self._running:
-            csock, addr = self._sock.accept()
-            log.debug("Client at %s", addr)
+    def __del__(self):
+        self._running = False
 
+    def accept_loop(self):
+        try:
+            log.info("Starting accept loop")
+            while self._running:
+                csock, addr = self._sock.accept()
+                key = struct.unpack('!40s', csock.recv(40))[0]
+                sizedata = csock.recv(struct.calcsize('h'))
+                size = int(struct.unpack('!h', sizedata)[0])
+                pobj = struct.unpack('!'+str(size)+'s', csock.recv(size))[0]
+                obj = pickle.loads(pobj)
+        finally:
+            log.info("Closing Socket")
+            self._sock.close()
+
+if __name__ == '__main__':
+    c = Chord()
+    c['trial'] = 45
