8112446fa87a1ef2d8cee53e5d24dff8431660bb
Justin Tulloss <jmtulloss@gmail.com>
2008-03-29 02:25:25 -0500

    Created an xml-rpc proxy to get this show on the road. I'm very tired now, so stopping, but it does properly publish all of a node's methods
    --HG--
    branch : trunk

--- chord/chord.py
+++ chord/chord.py
@@ -8,6 +8,7 @@ import logging
 import socket
 import struct
 from struct import Struct
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 import cPickle
 import threading
 import hashlib
@@ -76,7 +77,16 @@ class Node(object):
         """
         query this node for its predecessor
         """
-        pass
+        p = self;
+        while p.id < id<= p.successor.id:
+            p = p.closest_precceding_finger(id)
+        return p
+
+    def closest_preceding_finger(self, id):
+        for i in range(KSIZE-1, 0, -1):
+            if self.id < finger[i].id < id:
+                return finger[i]
+        return self
 
     def join(self, seedNode):
         if seedNode == None:
@@ -112,28 +122,27 @@ class Node(object):
 
     def get_successor(self):
         if self._successor == None:
-            self._successor = self.find_successor(self, self.id)
+            self._successor = self.find_successor(self.id)
         return self._successor
 
-    successor = property(get_successor, None, None, 
-        "The node adjacent to this node in the ring")
+    #successor = property(get_successor, None, None, 
+    #"The node adjacent to this node in the ring")
 
     def get_predecessor(self):
         if self._predecessor== None:
-            self._predecessor = self.find_predecessor(self, self.id)
+            self._predecessor = self.find_predecessor(self.id)
         return self._predecessor
 
     def set_predecessor(self, node):
         self._predecessor = node
         # Actually call the node and tell it that its predecessor has changed
 
-    predecessor = property(get_predecessor, set_predecessor, None, 
-        "The node behind this node in the ring")
+    #predecessor = property(get_predecessor, set_predecessor, None, 
+    #    "The node behind this node in the ring")
 
 
 class Chord(object):
     def __init__(self, seedNode=None, ip='127.0.0.1', port=4090):
-        self.packet = Struct('!bih')
         # Configure the logger
         logging.basicConfig(
             level = logging.DEBUG,
@@ -146,19 +155,18 @@ class Chord(object):
         # Create yourself!
         self.node = Node(ip = ip, port = port)
         self.node.join(seedNode)
-        # Initialize socket
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.bind(('0.0.0.0', port))
-        self._sock.listen(4)
+
+        # Initialize server
+        self._server = SimpleXMLRPCServer((ip, port))
+        self._server.register_introspection_functions()
+        self._server.register_instance(self.node)
 
         # Initialize thread to serve connections on said socket
-        self._running = True
         self._accept_thread = threading.Thread(None, self.accept_loop)
-        self._accept_thread.setDaemon(True)
+        #self._accept_thread.setDaemon(True)
         self._accept_thread.start()
 
     def __del__(self):
-        self._running = False
         self._log.info("Closing Socket")
         if self._sock != None:
             self._sock.close()
@@ -166,14 +174,7 @@ class Chord(object):
     def accept_loop(self):
         self._log.info("Starting accept loop")
         try:
-            while self._running:
-                csock, addr = self._sock.accept()
-                key = struct.unpack('!i', csock.recv(struct.calcsize('i')))[0]
-                sizedata = csock.recv(struct.calcsize('h'))
-                size = int(struct.unpack('!h', sizedata)[0])
-                pobj = struct.unpack('!'+str(size)+'s', csock.recv(size))[0]
-                obj = pickle.loads(pobj)
-                self._log.debug("Received packet: %d, %d, %s", key, size, obj)
+            self._server.serve_forever()
         except Exception, e:
             self._log.exception("Exception: %s", e)
             raise
