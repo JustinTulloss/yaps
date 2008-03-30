39360ef12f3670c4407f007eb5568f82923dcf52
Justin Tulloss <jmtulloss@gmail.com>
2008-03-30 13:07:11 -0500

    Pyro implemented a little better, but it's complaining about using proxies between threads (even though I don't think I am). Moving development back to the desktop
    --HG--
    branch : pyro

--- chord/chord.py
+++ chord/chord.py
@@ -9,6 +9,9 @@ import socket
 import struct
 from struct import Struct
 import Pyro.core
+import Pyro.util
+import Pyro
+Pyro.config.PYRO_MULTITHREADED = 0
 import cPickle
 import threading
 import hashlib
@@ -61,19 +64,18 @@ class Node(Pyro.core.ObjBase):
         Pyro.core.ObjBase.__init__(self)
         self.addr = (ip, port)
         self.id = hash(self.addr)
-        self.url = 'http://%s:%d' % self.addr
         self.ip = ip
         self.port = port
         self._successor = None
         self._predecessor = None
-        self.finger = range(0, KSIZE-1)
-        self.start =  int((self.id + 1) % 2**KSIZE)
+        self.finger = range(0, KSIZE)
+        self.start =  int((self.id + 1) % 2**(KSIZE-1))
 
     def find_successor(self, id):
         """
         query this node for a successor
         """
-        node = self.find_predecessor(id).successor
+        node = self.find_predecessor(id).finger[0]
         return node
 
     def find_predecessor(self, id):
@@ -81,65 +83,64 @@ class Node(Pyro.core.ObjBase):
         query this node for its predecessor
         """
         p = self;
-        while p.id < id <= p.successor.id:
-            p = p.closest_precceding_finger(id)
+        print p.id, id, p.finger[0].id
+        while not (p.id < id <= p.finger[0].id):
+            p = p.closest_preceding_finger(id)
         return p
 
     def closest_preceding_finger(self, id):
         for i in range(KSIZE-1, 0, -1):
-            if self.id < finger[i].id < id:
-                return finger[i]
+            if self.id < self.finger[i].id < id:
+                return self.finger[i]
         return self.getAttrProxy()
 
     def join(self, seedNode):
         # I know this looks bad, but using the type avoids an RPC request
-        if type(seedNode) != type(None):
+        if seedNode != None:
             self.init_finger_tables(seedNode)
+            self.update_others()
         else:
-            for i in range(0, KSIZE-1):
+            for i in range(0, KSIZE):
                 self.finger[i] = self.getAttrProxy()
-            self._predecessor = self.getAttrProxy()
-            self._successor = self.getAttrProxy()
+            self.predecessor = self.getAttrProxy()
 
     def init_finger_tables(self, seedNode):
         next_start = int((self.id+2)%2**(KSIZE-1))
-        self.finger[0] = seedNode.find_successor(next_start)
+        try:
+            self.finger[0] = seedNode.find_successor(next_start)
+        except Exception, e:
+            print ''.join(Pyro.util.getPyroTraceback(e))
         self.successor = self.finger[0]
-        self.predecessor = self.successor.predecessor
-        self.successor.predecessor = self.getAttrProxy()
-        for i in range(0, KSIZE-1):
+        self.predecessor = self.finger[0].predecessor
+        self.finger[0].predecessor = self.getAttrProxy()
+        for i in range(0, KSIZE-2):
+            next_start = int((self.id+2**i)%2**(KSIZE-1))
             if self.id <= next_start < self.finger[i].id:
-                self.finger[i+1].successor = finger[i].successor
+                self.finger[i+1] = self.finger[i]
             else:
-                next_start = int((self.id+2**i)%2**(KSIZE-1))
-                self.finger[i+1].successor = seedNode.find_successor(next_start)
-        print self.finger
+                self.finger[i+1] = seedNode.find_successor(next_start)
+        print "Fingers:"
+        for node in self.finger:
+            print node.id
 
     def update_others(self):
-        for i in range(1, KSIZE-1):
-            p = self.find_predecessor(self.id-2**(i-1))
-            p.update_finger_table(self, i)
+        for i in range(0, KSIZE):
+            print self.id, self.id-2**i
+            p = self.find_predecessor(self.id-2**(i))
+            print p.addr, self.addr
+            p.update_finger_table(self.getAttrProxy(), i)
 
 
     def update_finger_table(self, node, i):
-        if self.id <= node.id < finger[i].id:
+        print "Fingering"
+        print self.finger[i].id
+        print self.port
+        print node
+        if self.id <= node.id < self.finger[i].id:
             self.finger[i] = node
             self.predecessor.update_finger_table(node, i)
 
-    def get_successor(self):
-        #if self._successor == None:
-        #    self._successor = self.find_successor(self.id)
-        return self._successor
-
-    def set_successor(self, node):
-        self._successor = node
-
-    successor = property(get_successor, set_successor, None, 
-        "The node adjacent to this node in the ring")
-
     def get_predecessor(self):
-        #if self._predecessor== None:
-        #    self._predecessor = self.find_predecessor(self.id)
         return self._predecessor
 
     def set_predecessor(self, node):
@@ -177,29 +178,25 @@ class Chord(object):
         self._accept_thread = threading.Thread(None, self.accept_loop)
         #self._accept_thread.setDaemon(True)
         self._accept_thread.start()
+        print "really ought to be returning"
 
-    def __del__(self):
-        self._log.info("Closing Socket")
-        if self._sock != None:
-            self._sock.close()
-    
     def accept_loop(self):
         self._log.info("Starting accept loop")
         try:
+            print "Hi"
             self._daemon.requestLoop()
+            print "bye"
         except Exception, e:
             self._log.exception("Exception: %s", e)
             raise
-        finally:
-            self._log.info("Closing Socket")
-            self._sock.close()
 
 if __name__ == '__main__':
     import netifaces
-    ip = netifaces.ifaddresses('eth1')[netifaces.AF_INET][0]['addr']
+    ip = netifaces.ifaddresses('en1')[netifaces.AF_INET][0]['addr']
     c = Chord(ip = ip)
     known = Pyro.core.getAttrProxyForURI(c._uri)
     d = Chord(seedNode = known, ip = ip, port=4091)
+    e = Chord(seedNode = known, ip = ip, port=9000)
     #c['trial'] = 45
     import time
     time.sleep(1) # Let the daemon thread finish doing stuff
