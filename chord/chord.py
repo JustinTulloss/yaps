ea52d6866aa1613b28ba23eec29faa57f25654fd
Justin Tulloss <jmtulloss@gmail.com>
2008-03-29 19:35:11 -0500

    pyro is working well, need to initialize finger tables better. Off to Nicole's for dinner
    --HG--
    branch : pyro

--- chord/chord.py
+++ chord/chord.py
@@ -8,8 +8,7 @@ import logging
 import socket
 import struct
 from struct import Struct
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-from xmlrpclib import Server
+import Pyro.core
 import cPickle
 import threading
 import hashlib
@@ -57,8 +56,9 @@ class Chord(object):
         pass
 """
 
-class Node(object):
+class Node(Pyro.core.ObjBase):
     def __init__(self, ip, port):
+        Pyro.core.ObjBase.__init__(self)
         self.addr = (ip, port)
         self.id = hash(self.addr)
         self.url = 'http://%s:%d' % self.addr
@@ -74,7 +74,7 @@ class Node(object):
         query this node for a successor
         """
         node = self.find_predecessor(id).successor
-        return node.url
+        return node
 
     def find_predecessor(self, id):
         """
@@ -89,7 +89,7 @@ class Node(object):
         for i in range(KSIZE-1, 0, -1):
             if self.id < finger[i].id < id:
                 return finger[i]
-        return self
+        return self.getAttrProxy()
 
     def join(self, seedNode):
         # I know this looks bad, but using the type avoids an RPC request
@@ -97,23 +97,23 @@ class Node(object):
             self.init_finger_tables(seedNode)
         else:
             for i in range(0, KSIZE-1):
-                self.finger[i] = self
-            self._predecessor = self
-            self._successor = self
+                self.finger[i] = self.getAttrProxy()
+            self._predecessor = self.getAttrProxy()
+            self._successor = self.getAttrProxy()
 
     def init_finger_tables(self, seedNode):
-        self.finger[0] = self
         next_start = int((self.id+2)%2**(KSIZE-1))
-        self.successor = seedNode.find_successor(next_start)
-        self.predecessor = self.get_successor().get_predecessor()
-        print self.successor, type(self.predecessor)
-        self.successor.predecessor = self
+        self.finger[0] = seedNode.find_successor(next_start)
+        self.successor = self.finger[0]
+        self.predecessor = self.successor.predecessor
+        self.successor.predecessor = self.getAttrProxy()
         for i in range(0, KSIZE-1):
-            next_start = int((self.id+2**i)%2**(KSIZE-1))
             if self.id <= next_start < self.finger[i].id:
                 self.finger[i+1].successor = finger[i].successor
             else:
+                next_start = int((self.id+2**i)%2**(KSIZE-1))
                 self.finger[i+1].successor = seedNode.find_successor(next_start)
+        print self.finger
 
     def update_others(self):
         for i in range(1, KSIZE-1):
@@ -132,10 +132,7 @@ class Node(object):
         return self._successor
 
     def set_successor(self, node):
-        if type(node) == str:
-            self._successor = Server(node)
-        else:
-            self._successor = node
+        self._successor = node
 
     successor = property(get_successor, set_successor, None, 
         "The node adjacent to this node in the ring")
@@ -151,6 +148,9 @@ class Node(object):
     predecessor = property(get_predecessor, set_predecessor, None, 
         "The node behind this node in the ring")
 
+    def get_id(self):
+        return self.id
+
 
 class Chord(object):
     def __init__(self, seedNode=None, ip='127.0.0.1', port=4090):
@@ -165,12 +165,13 @@ class Chord(object):
 
         # Create yourself!
         self.node = Node(ip = ip, port = port)
-        self.node.join(seedNode)
 
         # Initialize server
-        self._server = SimpleXMLRPCServer((ip, port))
-        self._server.register_introspection_functions()
-        self._server.register_instance(self.node)
+        Pyro.core.initServer()
+        self._daemon = Pyro.core.Daemon(host=ip, port=port)
+        self._uri = self._daemon.connect(self.node)
+
+        self.node.join(seedNode)
 
         # Initialize thread to serve connections on said socket
         self._accept_thread = threading.Thread(None, self.accept_loop)
@@ -185,7 +186,7 @@ class Chord(object):
     def accept_loop(self):
         self._log.info("Starting accept loop")
         try:
-            self._server.serve_forever()
+            self._daemon.requestLoop()
         except Exception, e:
             self._log.exception("Exception: %s", e)
             raise
@@ -197,7 +198,7 @@ if __name__ == '__main__':
     import netifaces
     ip = netifaces.ifaddresses('eth1')[netifaces.AF_INET][0]['addr']
     c = Chord(ip = ip)
-    known = Server('http://'+ip+':4090', allow_none=True)
+    known = Pyro.core.getAttrProxyForURI(c._uri)
     d = Chord(seedNode = known, ip = ip, port=4091)
     #c['trial'] = 45
     import time
